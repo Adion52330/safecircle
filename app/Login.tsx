9bd081afca79c922aa25377dbee93c15383baaaf
Sid1803 <siddheshkmande@gmail.com>
2025-12-25 15:42:34 +0530

    setup: auth and login page

---
+++ app/Login.tsx
@@ -0,0 +1,119 @@
+import { useRouter } from "expo-router";
+import { createUserWithEmailAndPassword, onAuthStateChanged, sendEmailVerification, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { ActivityIndicator, Alert, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { auth } from "../firebaseConfig";
+import "../global.css";
+
+const Login = () => {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [isLoginMode, setIsLoginMode] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user && user.emailVerified) {
+        router.replace("/Home");
+      }
+    });
+    return unsubscribe;
+  }, []);
+
+  const handleLogin = async () => {
+    setLoading(true);
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+      await user.reload();
+      if (!user.emailVerified) {
+        Alert.alert("Verification Required", 
+          "Please check inbox to complete verification and continue.",
+          [
+            { text: "OK"},
+            { text: "Resend Email", onPress: () => sendEmailVerification(user)}
+          ]
+        );
+        await signOut(auth);
+        return;
+      }
+
+      Alert.alert("Success!", "Logged in successfully.");
+
+    } catch (error: any) {
+      Alert.alert("Login failed!", error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleRegister = async () => {
+    setLoading(true);
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+
+      await sendEmailVerification(user);
+      await signOut(auth);
+      
+      Alert.alert("Success!", "Accounted created. Check inbox for verification.");
+
+    } catch (error: any) {
+      Alert.alert("Registration failed!", error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+
+  return (
+    <View className="flex flex-col bg-neutral-950 justify-center h-screen py-20 px-5">
+      <View className="bg-gray-700 rounded flex flex-col items-center gap-5 pb-4 h-full">
+        <Text className="text-white text-center p-2 text-3xl">
+          SafeCircle+
+        </Text>
+
+        <View className="flex flex-col w-full items-center gap-3">
+          <TextInput
+            placeholder="Email"
+            value={email}
+            onChangeText={setEmail}
+            keyboardType="email-address"
+            className="bg-white text-black rounded w-3/4 p-1"
+          />
+
+          <TextInput
+            placeholder="Password"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry
+            className="bg-white text-black rounded w-3/4 p-1"
+          />
+        </View>
+
+        {loading ? (<ActivityIndicator size="large" />) :
+
+          (isLoginMode ?
+
+            (<TouchableOpacity onPress={handleLogin} className="bg-pink-600 rounded p-2">
+              <Text className="text-white">Login</Text>
+            </TouchableOpacity>) :
+
+            (<TouchableOpacity onPress={handleRegister} className="bg-pink-600 rounded p-2">
+              <Text className="text-white">Sign up</Text>
+            </TouchableOpacity>))
+        }
+
+        <TouchableOpacity onPress={() => setIsLoginMode(!isLoginMode)}>
+          <Text className="text-white">
+            {isLoginMode ? "Need an account? Sign up" : "Have an account? Login"}
+          </Text>
+        </TouchableOpacity>
+
+      </View>
+    </View>
+  )
+}
+
+export default Login
---
+++ firebaseConfig.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { getReactNativePersistence, initializeAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = initializeAuth(app, { persistence: getReactNativePersistence(AsyncStorage)});
+
+export { auth };
